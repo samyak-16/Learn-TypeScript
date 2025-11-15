14d09b2cf076a2e999dcfe094231bae40a908986
samyak-16 <samexecodes@gmail.com>
2025-11-15 12:39:31 +0545

    Learnt about Enums and const with enums

---
+++ 02-basics/myEnums.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var samyakSeat = 11122 /* seatChoice.MIDDLE */;
+console.log(samyakSeat);
---
+++ 02-basics/myEnums.ts
@@ -0,0 +1,19 @@
+// enum seatChoice { 
+//     MIDDLE = 11122 , //Default value = 0
+//     WINDOW = 22254,  //Default value = 1
+//     AISLE = 2465463, //Default value = 2
+//     FOURTH = 41641564//Default value = 3
+//     // and soonn
+// } // This generates crazy js code so not recomended :)
+const enum seatChoice { 
+    MIDDLE = 11122 , //Default value = 0
+    WINDOW = 22254,  //Default value = 1
+    AISLE = 2465463, //Default value = 2
+    FOURTH = 41641564//Default value = 3
+    // and soonn
+}
+
+
+const samyakSeat =  seatChoice.MIDDLE
+console.log(samyakSeat);
+export{}
