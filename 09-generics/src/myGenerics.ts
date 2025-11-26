4f06e9b55fe2878190287e02f4c6f6e5b9976628
samyak-16 <samexecodes@gmail.com>
2025-11-26 23:30:15 +0545

    Learnt basic of generics and their use case

---
+++ 09-generics/src/myGenerics.ts
@@ -0,0 +1,31 @@
+const score :   Array<number> = []
+const names :   Array<string> = []
+console.log(score);
+
+
+
+function identityOne(val:boolean|number):boolean | number{
+return  val
+}
+
+function identityTwo(val:any):any{
+return  val
+}
+
+function identityThree<Type>(val : Type):Type{
+return val
+}
+identityThree("Samyak")
+function identityFour<T>(val : T):T{  // Shortcut syntax
+return val
+}
+identityFour("Pranika")
+
+interface Bottel {
+    brand : string  ; 
+    price : number ; 
+    type : string ; 
+}
+
+console.log(identityFour<Bottel>({brand : "cello" , price : 1400 , type: "Gym"}));
+
